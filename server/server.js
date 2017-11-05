2fa62ffce40848191d10b6109f37568d1d264146
kakula01 <keerthiakula1@gmail.com>
2017-11-06 05:01:38 +1100

    files uploading to git

--- server/server.js
+++ server/server.js
@@ -10,40 +10,31 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// app.post('/todo',(req,res)=>{
-// 	var todo = new Todo({
-// 		text: req.body.text
+app.post('/todos',(req,res)=>{
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc);
+	},(e)=>{
+		res.status(400).send(e);
+	})
+});
+
+// app.post('/users',(req,res)=>{
+// 	var user = new Users({
+// 		email: req.body.email
 // 	});
 
-// 	todo.save().then((doc)=>{
+// 	user.save().then((doc)=>{
 // 		res.send(doc);
-// 	},(e)=>{
-// 		res.status(400).send(e);
-// 	})
+// 	},()=>{
+// 		res.status.send(e);
+// 	});
 // });
-
-app.post('/users',(req,res)=>{
-	var user = new Users({
-		email: req.body.email
-	});
-
-	user.save().then((doc)=>{
-		res.send(doc);
-	},()=>{
-		res.status.send(e);
-	});
-});
 
 app.listen(4000,()=>{
 	console.log('started on port 4000')
 });
-
-// var firstUser = new Users({
-// 	//email:'keerthiakula1@gmail.com'
-// });
-
-// firstUser.save().then((doc)=>{
-// 	console.log('save first User',doc)
-// },(e)=>{
-// 	console.log('Error occured',e)
-// })
+module.exports ={Todo}
